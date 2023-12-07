308c31914d3ba010803dc662366f0e983409320f
Allen Febi M A <allenabi2614@gmail.com>
2023-12-07 23:11:54 +0530

    Portfolio

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,34 +1,46 @@
-<footer id="contact"
-    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+<footer
+  id="contact"
+  class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
-    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
-        Connect with me &darr;
+  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    Connect with me &darr;
+  </p>
+  <div class="flex flex-col gap-4 items-center justify-center">
+    <p>
+      <b class="pr-2 text-white">Email</b><a
+        class="text-violet-400"
+        href="mailto: allenabi2614@gmail.com">Mail Me!</a
+      >
     </p>
-    <div class="flex flex-col gap-4 items-center justify-center">
-        <p><b class="pr-2 text-white">Email</b><a class="text-violet-400" href = "mailto: allenabi2614@gmail.com">Mail Me!</a></p>
-        <p>
-            <b class="pr-2 text-white">GitHub</b>
-            <a href="https://github.com/all3n2601" target="_blank" class="text-violet-400"
-                >all3n2601<sup class=""
-                    ><span class="text-xs scale-75 pl-0.5"
-                        ><i
-                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
-                        /></span
-                    ></sup
-                ></a
-            >
-        </p>
-        <p>
-            <b class="pr-2 text-white">LinkedIn</b>
-            <a href="https://www.linkedin.com/in/all3n-f3bi/" target="_blank" class="text-violet-400"
-                >all3n-f3bi<sup class=""
-                    ><span class="text-xs scale-75 pl-0.5"
-                        ><i
-                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
-                        /></span
-                    ></sup
-                ></a
-            >
-        </p>
-    </div>
+    <p>
+      <b class="pr-2 text-white">GitHub</b>
+      <a
+        href="https://github.com/all3n2601"
+        target="_blank"
+        class="text-violet-400"
+        >all3n2601<sup class=""
+          ><span class="text-xs scale-75 pl-0.5"
+            ><i
+              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+            /></span
+          ></sup
+        ></a
+      >
+    </p>
+    <p>
+      <b class="pr-2 text-white">LinkedIn</b>
+      <a
+        href="https://www.linkedin.com/in/all3n-f3bi/"
+        target="_blank"
+        class="text-violet-400"
+        >all3n-f3bi<sup class=""
+          ><span class="text-xs scale-75 pl-0.5"
+            ><i
+              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+            /></span
+          ></sup
+        ></a
+      >
+    </p>
+  </div>
 </footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,20 +5,24 @@
 
   let y;
   let innerHeight = 0;
-  let innerWidth=0;
+  let innerWidth = 0;
 
-  function goTop(){
-    document.body.scrollIntoView()
+  function goTop() {
+    document.body.scrollIntoView();
   }
 </script>
 
 <div
   class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
-  <div class={"fixed bottom-0 w-full duration-200 p-10 flex z-[10] " + (
-    y > 0 ? 'opacity-full pointer-events-auto ' : 'pointer-events-none'
-  )}>
-    <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer">
+  <div
+    class={"fixed bottom-0 w-full duration-200 p-10 flex z-[10] " +
+      (y > 0 ? "opacity-full pointer-events-auto " : "pointer-events-none")}
+  >
+    <button
+      on:click={goTop}
+      class="ml-auto rounded-full bg-white text-violet-400 px-3 sm:px-4 hover:bg-violet-400 cursor-pointer"
+    >
       <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
     </button>
   </div>
@@ -27,4 +31,4 @@
   <Footer />
 </div>
 
-<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth/>
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
